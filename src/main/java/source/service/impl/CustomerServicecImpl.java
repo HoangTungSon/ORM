456a2902b976a8de0dae543bcc912ca35edc7d52
HoangTungSon <hoangtungson164@gmail.com>
2019-09-16 11:35:27 +0700

    first commit

---
+++ src/main/java/source/service/impl/CustomerServicecImpl.java
@@ -0,0 +1,36 @@
+package source.service.impl;
+
+import source.model.Customer;
+import source.repository.CustomerRepository;
+import source.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CustomerServicecImpl implements CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+
+    }
+
+    @Override
+    public void remove(Long id) {
+        customerRepository.remove(id);
+
+    }
+}
